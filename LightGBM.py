ea244d1d0d26640a52214867134584e2c9f63b99
GZK1108 <stanleywell1229@gmail.com>
2022-09-24 17:04:37 +0800

    1.1

--- LightGBM.py
+++ LightGBM.py
@@ -1,24 +1,36 @@
 import pandas as pd
+from lightgbm import plot_importance
 from matplotlib import pyplot as plt
 import lightgbm as lgb
 from sklearn.metrics import accuracy_score, roc_curve, recall_score, precision_score, auc
 import time
 from sklearn.model_selection import train_test_split
+
 # import joblib
 
 time_start = time.time()
-df = pd.read_csv('C:/Users/11453/PycharmProjects/riskassessment/data/creditrisk/creditdata.csv', header=1)
+df = pd.read_csv('C:/Users/11453/PycharmProjects/riskassessment/data/creditrisk/creditdata.csv', header=0)
+df_label = pd.read_csv('C:/Users/11453/PycharmProjects/riskassessment/data/creditrisk/creditdata.csv', header=0)
+df_label.drop(['TARGET'], axis=1, inplace=True)
+labels = list(df_label.columns.values)
+print(labels)
 
 y = df.iloc[:, 0]
 x = df.iloc[:, 1:]
+"""y = y.values
+x = x.values
+
+x = x.tolist()
+y = y.tolist()"""
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
 # train_x = train_x.values
 
+# lgb_train = lgb.Dataset(x_train, y_train, feature_name=labels)
 lgb_train = lgb.Dataset(x_train, y_train)
 lgb_eval = lgb.Dataset(x_test, y_test, reference=lgb_train)
 params = {
     'boosting_type': 'goss',  # gbdt使用树，goss使用单边梯度抽样算法，使用随机森林
-    'metric': {'12', 'auc', 'binary_logloss'},
+    'metric': 'auc',
     'use_missing': True,
     'learning_rate': 0.005134,
     'num_leaves': 54,
@@ -30,15 +42,19 @@ params = {
     'min_split_gain': 0.024766,
     'subsample': 1,
     'is_unbalance': True,
-    'silent':-1,
-    'verbose':-1
+    'silent': -1,
+    'verbose': -1
 }
 
-gbm = lgb.train(params, lgb_train, num_boost_round=1500, valid_sets=lgb_eval)
+gbm = lgb.train(params, lgb_train, num_boost_round=2000, valid_sets=lgb_eval, early_stopping_rounds=1500)
 # gbm = lgb.train(params, lgb_train, num_boost_round=1000, early_stopping_rounds=100)
+#  重命名
+# gbm.set_feature_name(labels)  # train为我的训练数据
+
+plot_importance(gbm, max_num_features=10)
+plt.show()
 
 y_pred = gbm.predict(x_test)
-
 
 fpr1, tpr1, thresholds1 = roc_curve(y_test, y_pred, pos_label=1)  # pos_label=1
 print("AUC为:")
