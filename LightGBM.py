e48a51091381c185acd7d3be067fe9251495f6c5
GZK1108 <stanleywell1229@gmail.com>
2022-10-10 17:11:42 +0800

    调参

--- LightGBM.py
+++ LightGBM.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import warnings
 import seaborn as sns
@@ -6,7 +7,7 @@ from matplotlib import pyplot as plt
 import lightgbm as lgb
 from sklearn.metrics import accuracy_score, roc_curve, recall_score, precision_score, auc
 import time
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 import joblib
@@ -36,8 +37,7 @@ def character():
     # 查找类型为非数值型标签
     df.select_dtypes('object').apply(pd.Series.nunique, axis=0)
 
-    # 使用LabelEncoder 转换为数值型
-    class_le = LabelEncoder()
+    # 使用LabelEncoder
 
     # 缺失值处理+编码
     for col in df:
@@ -94,9 +94,10 @@ def lightgbm(df):
     params = {
         'boosting_type': 'goss',  # gbdt使用树，goss使用单边梯度抽样算法，使用随机森林
         'metric': 'auc',
-        'learning_rate': 0.005,
-        'num_leaves': 54,
-        'max_depth': 10,
+        'objective': 'binary',
+        'learning_rate': 0.00545,
+        'num_leaves': 40,
+        'max_depth': 6,
         'subsample_for_bin': 240000,
         'reg_alpha': 0.44,
         'reg_lambda': 0.48,
@@ -104,12 +105,14 @@ def lightgbm(df):
         'min_split_gain': 0.024766,
         'subsample': 1,
         'is_unbalance': False,
-        'silent':-1,
-        'verbose':-1
+        'verbose': -1
     }
 
-    gbm = lgb.train(params, lgb_train, num_boost_round=3000, valid_sets=lgb_eval, early_stopping_rounds=2800)
-    # gbm = lgb.train(params, lgb_train, num_boost_round=1000, early_stopping_rounds=100)
+    # 训练
+    gbm = lgb.train(params, lgb_train, num_boost_round=5000, valid_sets=lgb_eval, early_stopping_rounds=4500)
+
+    # 交差检验
+    # gbmcv = lgb.cv(params, lgb_train, num_boost_round=5000, early_stopping_rounds=4500, nfold=5)
 
     # 输出特征重要性
 
@@ -127,19 +130,12 @@ def lightgbm(df):
     # feature_importance.to_csv('feature_importance.csv', index=False)
 
     # 删除低分特征并重新训练
-    """for i in range(len(feature_importance)):
-        if feature_importance['importance'][i] == 0:
-            df.drop([feature_importance['feature_name'][i]], axis=1, inplace=True)
-
-    y = df.iloc[:, 0]
-    x = df.iloc[:, 1:]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
-    lgb_train = lgb.Dataset(x_train, y_train)
-    lgb_eval = lgb.Dataset(x_test, y_test, reference=lgb_train)
-    gbm2 = lgb.train(params, lgb_train, num_boost_round=2000, valid_sets=lgb_eval, early_stopping_rounds=1500)"""
+    # for i in range(len(feature_importance)):
+    # if feature_importance['importance'][i] == 0:
+    # df.drop([feature_importance['feature_name'][i]], axis=1, inplace=True)
 
     # 检验
-    y_pred = gbm.predict(x_test)
+    y_pred = gbm.predict(x_test, num_iteration=gbm.best_iteration)
     fpr1, tpr1, thresholds1 = roc_curve(y_test, y_pred, pos_label=1)  # pos_label=1
     print("AUC为:", auc(fpr1, tpr1))
 
